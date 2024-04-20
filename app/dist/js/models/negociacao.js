73ecedb4d67d2bbb56e71af0e2b374672b792006
Leandro Rodrigues Pedroso <lrodriguespedroso@hotmail.com>
2024-04-20 19:03:30 -0300

    atualização para o curso final da formação

--- app/dist/js/models/negociacao.js
+++ app/dist/js/models/negociacao.js
@@ -12,9 +12,10 @@ export class Negociacao {
         return data;
     }
     static criaDe(dataString, quantidadeString, valorString) {
-        const data = new Date(dataString.replace(/-/g, ','));
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
         const quantidade = parseInt(quantidadeString);
         const valor = parseFloat(valorString);
-        return new Negociacao(data, quantidade, valor);
+        return new Negociacao(date, quantidade, valor);
     }
 }
